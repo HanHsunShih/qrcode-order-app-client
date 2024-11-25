a1728d0d83ef2036a059468a9e89755ff4a04a64
Han Hsun Shih (Amy) <a23514788@gmail.com>
2024-11-25 13:31:55 +0000

    create routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>welcome!!!!</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/menu" />
+          <Route path="/menu/:product_id" />
+          <Route path="/cart/:customer_id" />
+          <Route path="/order" />
+          <Route path="/order/:customer_id" />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
