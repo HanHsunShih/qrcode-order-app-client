01d738b846dfd2448f64e6cddb2f1a9f090f045c
Han Hsun Shih (Amy) <a23514788@gmail.com>
2025-02-11 23:31:53 +0000

    Adjust the time value again

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -55,7 +55,7 @@ export default function MenuPage({
       requestAnimationFrame(() => {
         window.scrollTo({ top: scrollPosition, behavior: "smooth" });
       });
-    }, 2000);
+    }, 500);
   };
 
   useEffect(() => {
@@ -73,7 +73,7 @@ export default function MenuPage({
           );
           window.scrollTo({ top: scrollPosition, behavior: "smooth" });
         });
-      }, 2000);
+      }, 500);
     }
   }, [cartInfo]);
 
